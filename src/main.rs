8b53aa2fc8e82656afb332b5d8a86f8817e710d7
Preetham Pemmasani <75422607+Preetham-ai@users.noreply.github.com>
2024-03-31 17:44:43 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,32 @@ use std::io::{self, stdin, stdout, Read};
 use crossterm::terminal;
 
 fn to_ctrl_byte(c: char) -> u8 {
-        let byte = c as u8;
-        byte & 0b0001_1111
-    }
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
 
-// fn die(err: std::io::Error) {
-//     panic!(err);
-// }
-
+fn die(err: std::io::Error) {
+    panic!("{}", err);
+}
 
 fn main() {
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
     for byte in stdin().bytes() {
-        let byte = byte.unwrap();
-        let chars = byte as char;
+        match byte {            
+            Ok(byte) => {
+                let byte = byte;
+                let chars = byte as char;
 
-        if chars.is_control() {
-            println!("{:?} \r", byte);
-        } else {
-            println!("{:?} ({})\r", byte, chars);
-        }
-        if byte == to_ctrl_byte('q') {
-            break;
+                if chars.is_control() {
+                    println!("{:?} \r", byte);
+                } else {
+                    println!("{:?} ({})\r", byte, chars);
+                }
+                if byte == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err), // Added match arm to handle Err case
         }
     }
     terminal::disable_raw_mode().expect("Could not turn off Raw mode");
